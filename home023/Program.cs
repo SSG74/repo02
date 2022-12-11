e6eed56b5e4bdfc170aaae926cada73d3d21bd78
Svutlana <fire_fly_74@mail.ru>
2022-12-11 13:51:47 +0500

    home023

---
+++ home023/Program.cs
@@ -0,0 +1,22 @@
+﻿// методы
+void Table(int N)
+{
+    int i = 1;
+    while (i <= N)
+    {
+        Console.Write(i*i*i+", ");
+        i++;
+    }   
+}
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+//запрос числа
+int N = EnterData("Введите число: ");
+//решение
+Table(N);
