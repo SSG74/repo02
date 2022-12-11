3dc55ff6d046757d7a78c013f42747d3bfca5249
Svutlana <fire_fly_74@mail.ru>
2022-12-11 17:22:04 +0500

    home021

---
+++ home021/Program.cs
@@ -0,0 +1,28 @@
+﻿// metod
+void rang(string str)
+{
+    if (str.Length ==5)
+    {
+        if (str[0] == str[4] && str[1] == str[3])
+        {
+            Console.WriteLine($"Число {str} является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine($"Число {str} не является палиндромом");
+        }
+    } 
+    else  
+    {  
+        Console.WriteLine($"Число {str} не является пятизначным");
+    }
+}
+string EnterData(string text)
+{
+    Console.Write(text);
+    string number = Console.ReadLine();
+    return number;
+}
+//запрос числа
+string N = EnterData("Введите пятизначное число: ");
+rang(N);
